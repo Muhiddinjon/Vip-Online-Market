b25b91dc4549f4d2573b13bab440d7d7a8029737
Olimxon <Olimhon>
2026-05-11 20:09:10 +0500

    343

--- app/Http/Controllers/Api/Auth/OtpController.php
+++ app/Http/Controllers/Api/Auth/OtpController.php
@@ -6,6 +6,9 @@ use App\Models\OtpCode;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Exception;
 
 class OtpController extends Controller
 {
@@ -20,6 +23,63 @@ class OtpController extends Controller
             'code'       => $code,
             'expires_at' => now()->addMinutes(5),
         ]);
+
+        try
+        {
+            $url = "https://send.smsxabar.uz/broker-api/send";
+
+            $headers = [
+                "Content-Type" => "application/json; charset=utf-8",
+                "Cache-Control" => "no-cache",
+                "Authorization" => "Basic cW9xb25zaXR5OmdINzl+WEo2eSpzXw=="
+            ];
+
+            // Remove leading '+998' if present
+            if (strpos($phone, '+998') !== false) {
+                $phone = substr($phone, 4);
+            }
+
+            $data = [
+                "messages" => [                   
+                    "recipient" => "998" . $phone,
+                    "message-id" => "ip" . $code,
+                    "sms" => [
+                        "originator" => "3700",
+                        "content" => [
+                            "text" => "Vip Online Market: Tasdiqlash kodi - " . $code
+                        ]
+                    ]
+                ]
+            ];
+
+            $client = new Client();
+
+            $promise = $client->postAsync($url, [
+                'headers' => $headers,
+                'json' => $data,
+                'timeout' => 3
+            ]);
+
+            // Attach success and error callbacks
+            $promise->then(
+                function ($response) {
+
+                },
+                function ($exception) {
+                    // Error callback
+                    // Handle the exception as needed
+                    \Log::error("An error occurred: " . $exception->getMessage());
+                }
+            );
+
+            $promise->wait();
+
+        }
+        catch (Exception $e) 
+        {
+            // Log the error
+            Log::error("An error occurred: " . $e->getMessage());
+        }
 
         // TODO: real SMS integration
         // SmsService::send($request->phone, "Tasdiqlash kodi: $code");
